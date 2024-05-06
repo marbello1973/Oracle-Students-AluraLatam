a9c24fe931cd8bfdfa315d8e74091331a10ff847
marbello1973 <mrbello1973>
2024-05-05 21:59:31 -0500

    actualizacion carpetas ebook

--- 12-Primer Proycto con Spring/screenmatch/src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
+++ 12-Primer Proycto con Spring/screenmatch/src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -7,7 +7,7 @@ import com.theokanning.openai.service.OpenAiService;
 public class ConsultaChatGPT {
     public static String obtenerTraduccion(String texto) {
 
-        OpenAiService service = new OpenAiService("sk-proj-jskPHFYfTqkb9022UtK1T3BlbkFJQC1DsMDH8i6d1y72BOxs");
+        OpenAiService service = new OpenAiService("API_KEY_CHAT_GPT");
 
         CompletionRequest requisicion = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
